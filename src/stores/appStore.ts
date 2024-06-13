f943c2df779a1d906678e6e4bd301a9db6e13dce
Leo Cheung <leozhang621@gmail.com>
2024-06-13 14:20:10 +0800

    update vite config

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -1,6 +1,15 @@
 import { defineStore } from 'pinia';
 
-export const useAppStore = defineStore('appStore', {
+import * as t from '@/types';
+import * as e from '@/types/enums';
+
+type State = {};
+
+type Actions = {};
+
+type Getters = {};
+
+export const useAppStore = defineStore<'appStore', State, Getters, Actions>('appStore', {
   state: () => ({
 
   }),
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import url from 'url';
 
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
 import VueComponent from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
 import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
 
@@ -37,6 +38,7 @@ const config = ({ mode, command }) => {
     },
     plugins: [
       vue(),
+      legacy(),
       createHtmlPlugin({
         minify: isOnline,
         inject: {
