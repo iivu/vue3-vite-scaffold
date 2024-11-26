16974a585e792c75e2511efdf505b66720887a62
Leo Cheung <leozhang621@gmail.com>
2024-11-26 16:08:28 +0800

    feat: 覆盖vue-query默认配置

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import type { VueQueryPluginOptions } from '@tanstack/vue-query';
 
 import { router } from './router';
 import App from './App.vue';
 import 'vant/es/toast/style';
 import 'vant/es/dialog/style';
 
+const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+      }
+    }
+  }
+}
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app
-  .use(VueQueryPlugin)
+  .use(VueQueryPlugin, vueQueryPluginOptions)
   .use(pinia)
   .use(router)
   .mount('#app');
